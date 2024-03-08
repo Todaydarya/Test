363f94963cd8913d746688e36f5be3a0fcc0a16d
Дарья Васильченко <todaydarya@gmail.com>
2024-03-08 15:05:48 +0300

    Добавьте файлы проекта.

---
+++ Test/Page/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Test.Model;
+
+namespace Test.Page
+{
+    /// <summary>
+    /// Логика взаимодействия для MainPage.xaml
+    /// </summary>
+    public partial class MainPage
+    {
+        private TestEntities dbContext = new TestEntities();
+        public MainPage()
+        {
+            InitializeComponent();
+            dataGridPackages.ItemsSource = dbContext.GlassPackages.ToList();
+        }
+
+        private void btnCheckArticle_Click(object sender, RoutedEventArgs e)
+        {
+            string article = txtArticle.Text;
+
+            try
+            {
+                var package = dbContext.GlassPackages.FirstOrDefault(p => p.Article == article);
+                if (package != null)
+                {
+                    dataGridPackages.ItemsSource = new[] { package };
+                }
+                else
+                {
+                    MessageBox.Show("Стеклопакет с указанным артикулом не найден.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка: " + ex.Message);
+            }
+        }
+    }
+}
